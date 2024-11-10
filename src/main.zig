bd9769ac618b5cd8508b0bcc3a46c7bd61b908d5
Rafael Viveiros <eu@rafaelv.com.br>
2024-11-10 13:18:31 -0300

    feat: v1 completa do jogo + testes

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,29 @@
 const std = @import("std");
+const root = @import("root.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const stdout = std.io.getStdOut().writer();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    try stdout.print("===============\nBem-vindo ao jogo do Jokenpô\n===============\n\nDigite:\n    1 - Pedra\n    2 - Papel\n    3 - Tesoura.\n Ou 0 - Fechar\n\n", .{});
+    try stdout.print("JOGADOR 'A' FAÇA SUA JOGADA: ", .{});
+    const inputPlayerA = try ask_user(u8);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("JOGADOR 'B' FAÇA SUA JOGADA: ", .{});
+    const inputPlayerB = try ask_user(u8);
 
-    try bw.flush(); // don't forget to flush!
+    const result = root.jokenpo(inputPlayerA, inputPlayerB);
+    // std.debug.print("\n\n{any}\n\n", .{result});
+
+    try stdout.print("\n\nO VENCEDOR FOI {any}\n\n", .{result});
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn ask_user(comptime number_type: type) !number_type {
+    const stdin = std.io.getStdIn().reader();
+    var buf: [10]number_type = undefined;
+
+    if (try stdin.readUntilDelimiterOrEof(buf[0..], '\n')) |user_input| {
+        return std.fmt.parseInt(number_type, user_input, 10);
+    } else {
+        return @as(number_type, 0);
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,180 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+// Jogo de Jokenpô
+// PlayerA e PlayerB escolhem entre Pedra, Papel, Tesoura
+// Calcular o resultado da rodada com base na regra:
+// - Pedra empata Pedra.
+// - Pedra perde Papel.
+// - Pedra ganha Tesoura.
+
+// - Papel ganha Pedra.
+// - Papel empata Papel.
+// - Papel perde Tesoura.
+
+// - Tesoura perde Pedra.
+// - Tesoura ganha Papel.
+// - Tesoura empata Tesoura.
+
+const Input = enum { Pedra, Papel, Tesoura };
+const Result = enum { Perde, Empate, Ganha };
+const WinnerResult = enum { Empate, PlayerA, PlayerB };
+const ErrorNotImplemented = error{};
+const Error = error{UserInput};
+
+fn calcularResultadoRodada(playerA: Input, playerB: Input) Result {
+    return switch (playerA) {
+        .Pedra => switch (playerB) {
+            .Pedra => Result.Empate,
+            .Papel => Result.Perde,
+            .Tesoura => Result.Ganha,
+        },
+        .Papel => switch (playerB) {
+            .Pedra => Result.Ganha,
+            .Papel => Result.Empate,
+            .Tesoura => Result.Perde,
+        },
+        .Tesoura => switch (playerB) {
+            .Pedra => Result.Perde,
+            .Papel => Result.Ganha,
+            .Tesoura => Result.Empate,
+        },
+    };
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test "Dado que PlayerA colocou Pedra e PlayerB colocou Pedra resultado é Empate" {
+    const inputPlayerA = Input.Pedra;
+    const inputPlayerB = Input.Pedra;
+
+    const resultado = calcularResultadoRodada(inputPlayerA, inputPlayerB);
+
+    // std.debug.print("resultado ==>> {}\n", .{resultado});
+
+    try testing.expectEqual(Result.Empate, resultado);
+}
+
+test "Dado que PlayerA colocou Pedra e PlayerB colocou Papel resultado é Perde" {
+    const inputPlayerA = Input.Pedra;
+    const inputPlayerB = Input.Papel;
+
+    const resultado = calcularResultadoRodada(inputPlayerA, inputPlayerB);
+
+    // std.debug.print("resultado ==>> {}\n", .{resultado});
+
+    try testing.expectEqual(Result.Perde, resultado);
+}
+
+test "Dado que PlayerA colocou Pedra e PlayerB colocou Tesoura resultado é Ganha" {
+    const inputPlayerA = Input.Pedra;
+    const inputPlayerB = Input.Tesoura;
+
+    const resultado = calcularResultadoRodada(inputPlayerA, inputPlayerB);
+
+    // std.debug.print("resultado ==>> {}\n", .{resultado});
+
+    try testing.expectEqual(Result.Ganha, resultado);
+}
+
+test "Dado que PlayerA colocou Papel e PlayerB colocou Pedra resultado é Ganha" {
+    const inputPlayerA = Input.Papel;
+    const inputPlayerB = Input.Pedra;
+
+    const resultado = calcularResultadoRodada(inputPlayerA, inputPlayerB);
+
+    // std.debug.print("resultado ==>> {}\n", .{resultado});
+
+    try testing.expectEqual(Result.Ganha, resultado);
+}
+
+test "Dado que PlayerA colocou Papel e PlayerB colocou Papel resultado é Empate" {
+    const inputPlayerA = Input.Papel;
+    const inputPlayerB = Input.Papel;
+
+    const resultado = calcularResultadoRodada(inputPlayerA, inputPlayerB);
+
+    // std.debug.print("resultado ==>> {}\n", .{resultado});
+
+    try testing.expectEqual(Result.Empate, resultado);
+}
+
+test "Dado que PlayerA colocou Papel e PlayerB colocou Tesoura resultado é Perde" {
+    const inputPlayerA = Input.Papel;
+    const inputPlayerB = Input.Tesoura;
+
+    const resultado = calcularResultadoRodada(inputPlayerA, inputPlayerB);
+
+    // std.debug.print("resultado ==>> {}\n", .{resultado});
+
+    try testing.expectEqual(Result.Perde, resultado);
+}
+
+test "Dado que PlayerA colocou Tesoura e PlayerB colocou Pedra resultado é Perde" {
+    const inputPlayerA = Input.Tesoura;
+    const inputPlayerB = Input.Pedra;
+
+    const resultado = calcularResultadoRodada(inputPlayerA, inputPlayerB);
+
+    // std.debug.print("resultado ==>> {}\n", .{resultado});
+
+    try testing.expectEqual(Result.Perde, resultado);
+}
+
+test "Dado que PlayerA colocou Tesoura e PlayerB colocou Papel resultado é Ganha" {
+    const inputPlayerA = Input.Tesoura;
+    const inputPlayerB = Input.Papel;
+
+    const resultado = calcularResultadoRodada(inputPlayerA, inputPlayerB);
+
+    // std.debug.print("resultado ==>> {}\n", .{resultado});
+
+    try testing.expectEqual(Result.Ganha, resultado);
+}
+
+test "Dado que PlayerA colocou Tesoura e PlayerB colocou Tesoura resultado é Empate" {
+    const inputPlayerA = Input.Tesoura;
+    const inputPlayerB = Input.Tesoura;
+
+    const resultado = calcularResultadoRodada(inputPlayerA, inputPlayerB);
+
+    // std.debug.print("resultado ==>> {}\n", .{resultado});
+
+    try testing.expectEqual(Result.Empate, resultado);
+}
+
+pub fn jokenpo(inputPlayerA: u8, inputPlayerB: u8) WinnerResult {
+    // std.debug.print("RESULTADO ==>> {!d} {!d}\n", .{ inputPlayerA, inputPlayerB });:A
+    const playerA = parse_input(inputPlayerA);
+    // std.debug.print("A: {any}\n", .{playerA});
+    const playerB = parse_input(inputPlayerB);
+    // std.debug.print("B: {any}\n", .{playerB});
+
+    const resultado = calcularResultadoRodada(playerA, playerB);
+    return switch (resultado) {
+        Result.Empate => WinnerResult.Empate,
+        Result.Ganha => WinnerResult.PlayerA,
+        Result.Perde => WinnerResult.PlayerB,
+    };
+}
+
+fn parse_input(input: u8) Input {
+    return switch (input) {
+        1 => Input.Pedra,
+        2 => Input.Papel,
+        3 => Input.Tesoura,
+        else => Input.Pedra,
+    };
+}
+
+test "User INPUT" {
+    std.debug.print("\n\nTESTES {s}\n\n", .{"User INPUT"});
+
+    var r = jokenpo(1, 1);
+    // std.debug.print("{any}", .{r});
+    try testing.expectEqual(WinnerResult.Empate, r);
+
+    r = jokenpo(1, 3);
+    try testing.expectEqual(WinnerResult.PlayerA, r);
+
+    r = jokenpo(1, 2);
+    try testing.expectEqual(WinnerResult.PlayerB, r);
 }
